c5713df04a8919065842876880d17a3d91bb88a0
Annu Sharma <annu7418@gmail.com>
2021-08-07 13:43:31 +0530

    First Time Push Request

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <CalendarTodayIcon />
+        eidetic
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
